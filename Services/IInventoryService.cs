373d8103f6f708d92161870c4962f55e4fb157b5
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2025-12-16 09:24:59 +0100

    Delete Services/IInventoryService.cs

--- Services/IInventoryService.cs
+++
@@ -1,9 +0,0 @@
-using PureStackLogistics.Models;
-
-namespace PureStackLogistics.Services;
-
-public interface IInventoryService
-{
-    Task<Product> AddProductAsync(Product product);
-    Task<IEnumerable<Product>> GetStockAsync(string? category);
-}

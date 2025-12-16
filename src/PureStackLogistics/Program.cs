3cf6acbbbb76dfe65ca6bded7c991a133e265bfd
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2025-12-16 09:52:22 +0100

    Update Program.cs

--- src/PureStackLogistics/Program.cs
+++ src/PureStackLogistics/Program.cs
@@ -4,21 +4,19 @@ using PureStackLogistics.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Add Services (Dependency Injection)
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// 2. Setup In-Memory Database
+// In-Memory Database
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("PureStackDb"));
 
-// 3. Register Business Services
+// Dependency Injection
 builder.Services.AddScoped<IInventoryService, InventoryService>();
 
 var app = builder.Build();
 
-// 4. Configure Pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -27,5 +25,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();

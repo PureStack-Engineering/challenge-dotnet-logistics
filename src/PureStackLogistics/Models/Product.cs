9e322329c595558497fac1635ff1da8d2a295b6b
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2025-12-17 09:55:44 +0100

    Update Product.cs

--- src/PureStackLogistics/Models/Product.cs
+++ src/PureStackLogistics/Models/Product.cs
@@ -3,8 +3,14 @@ namespace PureStackLogistics.Models;
 public class Product
 {
     public int Id { get; set; }
+    
     public string Name { get; set; } = string.Empty;
+    
     public decimal Price { get; set; }
+    
     public int Stock { get; set; }
+    
     public string Category { get; set; } = string.Empty;
+    
+    public bool IsHazardous { get; set; }
 }
